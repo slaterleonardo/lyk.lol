9fd14469fd5d95dced827b6ff0cb9cee82f4b6bb
Slater Leonardo <147994094+slaterleonardo@users.noreply.github.com>
2023-11-02 12:10:49 -0400

    Update Link.js

--- models/Link.js
+++ models/Link.js
@@ -1,25 +1,25 @@
 const mongoose = require("mongoose");
 
 const linkSchema = new mongoose.Schema({
-  _id: {
+    _id: {
         type: String,
-      },
-  original_link: {
-    type: String,
-    required: true,
-  },
-  clicks: {
-    type: Number,
-    default: 0,
-  },
-  created_at: {
-    type: Date,
-    default: Date.now,
-  },
+    },
+    original_link: {
+        type: String,
+        required: true,
+    },
+    clicks: {
+        type: Number,
+        default: 0,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
 },
-  {
-    versionKey: false,
-  },
+    {
+        versionKey: false,
+    },
 );
 
 module.exports = mongoose.model("Link", linkSchema);
